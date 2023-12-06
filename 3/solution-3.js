290762ca31db8e3bfe78ff6c9c1520035ca50a82
Joe Ostrow <josephostrow@Joes-MacBook-Air.local>
2023-12-06 14:54:55 -0500

    problem 3 part 2 first attempt

--- 3/solution-3.js
+++ 3/solution-3.js
@@ -32,17 +32,20 @@ async function processLines() {
     lineNum++;
   }
 
-  // console.log(uniqueSymbols);
-
   return getSumPartNums(obj, symbols);
   // return getSumGearRatios(obj, symbols);
 }
 
 function getSumPartNums(data, symbols) {
   let sumPartNums = 0;
+  let sumGearRatios = 0;
+  // all gearIndices that curNum touches
+  let allGearParts = {};
 
   for (let row in data) {
     let numArr = [];
+    // part 2:
+    let gearIndices = [];
     let currentNum = '';
     let line = data[row];
     let validPart = false;
@@ -67,6 +70,14 @@ function getSumPartNums(data, symbols) {
         // if there's a gear above, add index of gear to gearIndices (for that num's index) {gearIdx1: [num], gearIdx2, [num, num] }
         if (above || below) validPart = true;
 
+        // part 2:
+        if (rowAbove && rowAbove[index] === '*') {
+          gearIndices.push(`r${row - 1}l${index}`);
+        }
+        if (rowBelow && rowBelow[index] === '*') {
+          gearIndices.push(`r${row + 1}l${index}`);
+        }
+
         // if first num, check prev, diagonal above prev, diagonal below prev
         if (currentNum.length === 1) {
           let left = line[index - 1] && symbols.has(line[index - 1]);
@@ -74,6 +85,17 @@ function getSumPartNums(data, symbols) {
           let belowLeft = rowBelow && symbols.has(rowBelow[index - 1]);
           if (left || aboveLeft || belowLeft) {
             validPart = true;
+          }
+
+          // part 2:
+          if (line[index - 1] === '*') {
+            gearIndices.push(`r${row}l${index - 1}`);
+          }
+          if (rowAbove && rowAbove[index - 1] === '*') {
+            gearIndices.push(`r${row - 1}l${index - 1}`);
+          }
+          if (rowBelow && rowBelow[index - 1] === '*') {
+            gearIndices.push(`r${row + 1}l${index - 1}`);
           }
         }
 
@@ -85,14 +107,34 @@ function getSumPartNums(data, symbols) {
           if (right || aboveRight || belowRight) {
             validPart = true;
           }
+
+          // part 2:
+          if (line[index + 1] === '*') {
+            gearIndices.push(`r${row}l${index + 1}`);
+          }
+          if (rowAbove && rowAbove[index + 1] === '*') {
+            gearIndices.push(`r${row - 1}l${index + 1}`);
+          }
+          if (rowBelow && rowBelow[index + 1] === '*') {
+            gearIndices.push(`r${row + 1}l${index + 1}`);
+          }
         }
       }
       //  check if currentNum is finished: 1) current value is a symbol OR 2) next index doesn't exist
       if (symbols.has(value) || !line.hasOwnProperty(index + 1)) {
         if (currentNum !== '' && validPart) numArr.push(parseInt(currentNum));
         // reset currentNum and validPart
+
+        // for each adjacent gear, populate numbers at the gear's index in allGearParts
+        for (const gear of gearIndices) {
+          allGearParts[gear] = allGearParts[gear]
+            ? [...allGearParts[gear], currentNum]
+            : [currentNum];
+        }
+
         currentNum = '';
         validPart = false;
+        gearIndices = [];
       }
     }
     // console.log(numArr);
@@ -100,5 +142,16 @@ function getSumPartNums(data, symbols) {
     sumPartNums += arrTotal;
   }
 
+  // console.log(allGearParts);
+
+  for (const gearPart in allGearParts) {
+    let parts = allGearParts[gearPart];
+    let product = parts[0] * parts[1];
+    if (parts.length === 2) {
+      sumGearRatios += product;
+    }
+  }
+
+  console.log('sumGearRatios', sumGearRatios);
   return sumPartNums;
 }
